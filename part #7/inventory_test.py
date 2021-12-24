a50275bf7c4e7e5ce8119c6a2c055e402dbec833
hanzo-0102 <naumov1vladimir@gmail.com>
2021-12-24 21:02:52 +0300

    New magicwand images, now inventory using in game, but have not functional. List of bugs : 1. Enemies doesn't move after spawning 2. Invantory has some delaing before closing Ideas : 1. May be add loading screen while closing inventory (it will delete 1 bug :) )

--- part #7/inventory_test.py
+++ part #7/inventory_test.py
@@ -2,9 +2,7 @@ import pygame
 import copy
 import os
 import sys
-
-
-balance = 0
+from collections import deque
 
 
 def load_image(name, colorkey=None):
@@ -23,206 +21,79 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Board:
-    def __init__(self, width, height):
+class Inventory:
+    def __init__(self, width, height, top, left, cellsize, player, sprites):
+        self.player = player
+        self.sprites = sprites
         self.width = width
         self.height = height
-        self.board = [[0 for i in range(width)] for j in range(height)]
-
-        self.left = 30
-        self.top = 30
-        self.cell_size = 33
-
-    def view(self, left, top, cell_size):
-        self.left = left
+        self.moving = False
+        self.invent = [[False for i in range(width)] for j in range(height)]
         self.top = top
-        self.cell_size = cell_size
+        self.left = left
+        self.cellsize = cellsize
+
+    def additem(self, item):
+        for x in range(len(self.invent)):
+            for y in range(len(self.invent[x])):
+                if not(self.invent[x][y]):
+                    self.invent[x][y] = item
+                    break
+        else:
+            self.dropitem(item, self.sprites, self.player)
+
+    def dropitem(self, item):
+        x = {'sprite': pygame.image.load(f'sprites/items/base/{item}.png').convert_alpha(),
+                'viewing_angles': False,
+                'shift': 0.8,
+                'scale': (0.8, 0.8),
+                'side': 30,
+                'animation': [],
+                'death_animation': deque([]),
+                'is_dead': 'immortal',
+                'dead_shift': 0.8,
+                'animation_dist': None,
+                'animation_speed': 6,
+                'blocked': True,
+                'flag': 'decor',
+                'obj_action': deque([])}
+        self.sprites.list_of_objects.append(x, self.player.pos)
+
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell, mouse_pos)
+
+    def get_cell(self, pos):
+        cell = ((pos[0] - self.left) / self.cellsize, (pos[1] - self.top) / self.cellsize)
+        if cell[0] < 0 or cell[0] > self.width or cell[1] < 0 or cell[1] > self.height:
+            return None
+        else:
+            return (int(cell[0]), int(cell[1]))
 
     def render(self, screen):
         for y in range(self.height):
             for x in range(self.width):
-                screen.blit(load_image('emptyslot.png'),
-                            (x * self.cell_size + self.left, y * self.cell_size + self.top))
+                screen.blit(pygame.image.load("sprites/inventory/emptyslot.png"),
+                            (self.left + (x * self.cellsize), self.top + (y * self.cellsize)))
+                if self.invent[x][y]:
+                    screen.blit(pygame.image.load(f"sprites/inventory/{self.invent[x][y]}.png"),
+                                (self.left + 2 + (x * self.cellsize), self.top + 2 + (y * self.cellsize)))
 
-    def get_cell(self, mouse_pos):
-        x, y = mouse_pos
-        if not(self.left <= x <= self.left + self.width * self.cell_size) or \
-                not(self.top <= y <= self.top + self.height * self.cell_size):
-            return None
-        x -= self.left
-        y -= self.top
-        return x // self.cell_size, y // self.cell_size
+    def on_click(self, cell, pos):
+        if self.moving == False:
+            self.dx = cell[0] * self.cellsize + self.left - pos[0]
+            self.dy = cell[1] * self.cellsize + self.top - pos[1]
+            self.invent[cell[0]][cell[1]] = False
+        else:
+            x, y = self.get_cell(pos)
+            if self.invent[x][y]:
+                self.dropitem(self.moving)
+            else:
+                self.invent[x][y] = self.moving
+            self.moving = False
 
-    def on_click(self, cell):
-        pass
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-
-
-class Icon(pygame.sprite.Sprite):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args)
-        self.image_name = kwargs["image"]
-        self.image = load_image(kwargs["image"])
-        self.rect = self.image.get_rect()
-        self.type = kwargs["type"]
-
-    def up(self, pos, inv):
-        x, y = pos
-        if self.rect.y <= y <= self.rect.y + self.rect.height and \
-                self.rect.x <= x <= self.rect.width + self.rect.x:
-            inv.current_image = self
-            inv.render(screen)
-            print(f"Вы перешли во вкладку {self.type}")
-
-
-pygame.init()
-size = width, height = 1250, 650
-screen = pygame.display.set_mode(size)
-
-all_sprites = pygame.sprite.Group()
-
-icon_swords = Icon(image="Sword_table_icon.jpg", type="swords")
-
-icon_potions = Icon(image="Potions_table_icon.jpg", type="potions")
-
-
-class Item:
-    def __init__(self, image, type_of_item, price=0):
-        self.image = load_image(image, colorkey=-1)
-        self.image_name = image
-        self.sprite = pygame.sprite.Sprite()
-        self.sprite.image = self.image
-        self.sprite.rect = self.sprite.image.get_rect()
-        self.type_item = type_of_item
-        self.price = price
-
-
-class Inventory(Board):
-    def __init__(self, width, height, icons):
-        super().__init__(width, height)
-        self.icons = [Icon(image=i.image_name, type=i.type) for i in icons]
-        self.current_image = self.icons[0]
-        self.inv_collection = [[None for i in range(width)] for j in range(height)]
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-        for i in self.icons:
-            i.up(mouse_pos, self)
-
-    def take_item(self, item):
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.inv_collection[i][j] is None:
-                    self.inv_collection[i][j] = Item(image=item.image_name,
-                                                     type_of_item=item.type_item, price=item.price)
-                    self.render(screen)
-                    return
-
-    def render(self, screen):
-        super().render(screen)
-        for i in range(len(self.icons)):
-            all_sprites.add(self.icons[i])
-            self.icons[i].rect.x = self.left - 30
-            if i > 0:
-                self.icons[i].rect.x = self.icons[i - 1].rect.x + self.icons[i - 1].rect.width
-            self.icons[i].rect.y = self.top - 30
-        k = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.inv_collection[i][j] is not None:
-                    all_sprites.remove(self.inv_collection[i][j].sprite)
-        for i in range(len(self.inv_collection)):
-            for j in range(len(self.inv_collection[i])):
-                if self.inv_collection[i][j] is not None:
-                    if self.inv_collection[i][j].type_item == self.current_image.type:
-                        sprite = self.inv_collection[i][j].sprite
-                        sprite.rect.x = self.left + self.cell_size * j
-                        sprite.rect.y = self.top + self.cell_size * i
-                        k += 1
-                        all_sprites.add(sprite)
-        all_sprites.draw(screen)
-
-    def destroy_item(self, cell):
-        x, y = cell[0], cell[1]
-        if self.inv_collection[y][x] is not None:
-            all_sprites.remove(self.inv_collection[y][x].sprite)
-            self.inv_collection[y][x] = None
-        self.render(screen)
-
-    def on_click(self, cell):
-        if cell is not None:
-            self.destroy_item(cell)
-
-
-class Merchantry:
-    def __init__(self, player_inventory, balance_m, name):
-        self.inventory = Inventory(10, 10, copy.copy([icon_swords, icon_potions]))
-        self.inventory.left = 650
-        self.name = name
-        self.player_inventory = player_inventory
-        self.balance = balance_m
-
-    def get_cell(self, mouse_pos):
-        x, y = mouse_pos
-        if not (self.inventory.left <= x <= self.inventory.left + self.inventory.width * self.inventory.cell_size) or \
-                not (self.inventory.top <= y <= self.inventory.top + self.inventory.height * self.inventory.cell_size):
-            return None
-        x -= self.inventory.left
-        y -= self.inventory.top
-        return x // self.inventory.cell_size, y // self.inventory.cell_size
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-        for i in self.inventory.icons:
-            i.up(mouse_pos, self.inventory)
-
-    def on_click(self, cell):
-        if cell is not None:
-            t = copy.copy(self.inventory.inv_collection[cell[1]][cell[0]])
-            if t is not None:
-                self.inventory.destroy_item(cell)
-                self.player_inventory.take_item(t)
-                print(f"Обмен осуществлён между {self.name} и Player")
-
-
-inventory = Inventory(10, 10, [icon_swords, icon_potions])
-
-temp = Item("knife.jpg", "swords")
-
-inventory.take_item(temp)
-
-font = pygame.font.Font(None, 50)
-text = font.render(str(balance), True, (255, 255, 255))
-text_x, text_y = 500, 10
-text_w, text_h = text.get_width(), text.get_height()
-screen.blit(text, (text_x, text_y))
-
-p = Item("Potion_healthy.jpg", "potions")
-
-inventory.take_item(p)
-
-Luis = Merchantry(inventory, 1000, "Luis")
-Luis.inventory.current_image = Luis.inventory.icons[1]
-Luis.inventory.take_item(p)
-Luis.inventory.render(screen)
-
-all_sprites.draw(screen)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            inventory.get_click(event.pos)
-            Luis.get_click(event.pos)
-
-    inventory.render(screen)
-    Luis.inventory.render(screen)
-    pygame.display.flip()
-
-pygame.quit()
+    def draw(self, screen):
+        if self.moving:
+            x, y = pygame.mouse.get_pos()
+            screen.blit(pygame.image.load(f"sprites/items/{self.moving}.png"), (x + self.dx, y + self.dy))
--- part #7/main.py
+++ part #7/main.py
@@ -6,6 +6,7 @@ from ray_casting import ray_casting_walls
 from drawing import Drawing
 from interaction import Interaction
 from map import world_map
+from inventory_test import Inventory
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,10 +18,12 @@ clock = pygame.time.Clock()
 player = Player(sprites)
 drawing = Drawing(sc, sc_map, player, clock, sc_gui)
 interaction = Interaction(player, sprites, drawing)
+inventory = Inventory(10, 10, 10, 300, 36, player, sprites)
+mode = 'game'
 drawing.menu()
 pygame.mouse.set_visible(False)
 interaction.play_music()
-
+waiting = 0
 while True:
     pygame.mixer.music.set_volume(VOLUME / 100)
 
@@ -33,24 +36,40 @@ while True:
         drawing.curwep = 1
         player.curwep = 1
         player.weapon = player.weapons[player.curwep]
-    player.movement()
-    drawing.background(player.angle)
-    walls, wall_shot = ray_casting_walls(player, drawing.textures)
-    drawing.world(walls + [obj.object_locate(player) for obj in sprites.list_of_objects])
-    drawing.fps(clock)
-    drawing.mini_map(player, sprites)
-    drawing.gui(player)
-    drawing.player_weapon([wall_shot, sprites.sprite_shot])
+    if keys[pygame.K_e] and waiting == 0:
+        mode = 'inventory' if mode == 'game' else 'game'
+        waiting = 100
+    elif waiting > 1:
+        waiting -= 1
+    elif keys[pygame.K_e] and waiting == 1:
+        waiting -= 1
+    if mode == 'game':
+        pygame.mouse.set_visible(False)
+        player.movement()
+        drawing.background(player.angle)
+        walls, wall_shot = ray_casting_walls(player, drawing.textures)
+        drawing.world(walls + [obj.object_locate(player) for obj in sprites.list_of_objects])
+        drawing.fps(clock)
+        drawing.mini_map(player, sprites)
+        drawing.gui(player)
+        drawing.player_weapon([wall_shot, sprites.sprite_shot])
 
-    for i in sprites.list_of_objects:
-        if not(i.is_dead):
-            if (abs(i.pos[0] - player.x) + abs(i.pos[1] - player.y) < 170):
-                player.dmg(i.damag)
+        for i in sprites.list_of_objects:
+            if not(i.is_dead):
+                if (abs(i.pos[0] - player.x) + abs(i.pos[1] - player.y) < 170):
+                    player.dmg(i.damag)
 
-    sprites.clearing(player, world_map)
-    interaction.interaction_objects()
-    interaction.npc_action()
-    interaction.clear_world()
+        sprites.clearing(player, world_map)
+        interaction.interaction_objects()
+        interaction.npc_action()
+        interaction.clear_world()
+    elif mode == 'inventory':
+        pygame.mouse.set_visible(True)
+        sc.blit(pygame.image.load('img/inventory.jpg'), (0, 0))
+        inventory.render(sc)
+        inventory.draw(sc)
+        sprites.list_of_objects = inventory.sprites.list_of_objects
+
 
     pygame.display.flip()
     clock.tick(FPS)
