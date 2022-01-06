69bf404aeeab9970ff125f62d65c1b23685f70c3
hanzo-0102 <naumov1vladimir@gmail.com>
2022-01-06 10:25:45 +0300

    Added new npc (blacksmith), new weapons (crashed iron sword, and (not crashed) iron sword). Bow again requiered mana for using

--- part #7/inventory_test.py
+++ part #7/inventory_test.py
@@ -25,7 +25,7 @@ items_rare = {
 def load_image(name, colorkey=None):
     fullname = os.path.join('Icons', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f"Файл с изображением'{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     if colorkey is not None:
